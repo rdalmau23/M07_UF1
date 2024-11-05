3704ba6b6bab4df5573a1299dd7aed30994669a3
Rafel Dalmau <145371879+rdalmau23@users.noreply.github.com>
2024-11-05 19:05:36 +0100

    Delete ACTIVITAT3 directory

--- ACTIVITAT3/.history/ex10_20241028112602.py
+++
@@ -1,8 +0,0 @@
-import string
-
-abecedari = list(string.ascii_uppercase)
-llista_filtrada = [lletra for i, lletra in enumerate(abecedari, start=1) if i % 3 != 0]
-tupla_resultant = tuple(llista_filtrada)
-
-print("Llista:", llista_filtrada)
-print("Tupla:", tupla_resultant)
--- ACTIVITAT3/.history/ex9_20241028112524.py
+++
@@ -1,9 +0,0 @@
-assignatures = ["Matemàtiques", "Física", "M7", "Català", "Anglès", "M3"]
-notes = []
-
-for assignatura in assignatures:
-    nota = float(input(f"Indica la nota de {assignatura}: "))
-    notes.append(nota)
-
-for i in range(len(assignatures)):
-    print(f"A {assignatures[i]} ha tret {notes[i]}")
