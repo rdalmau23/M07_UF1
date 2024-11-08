c0ae988c66dc9383fb83dde0fa70c63e246d5912
rdalmau23 <rdalmau23@ilg.cat>
2024-11-08 10:11:40 +0100

    rr

--- ACTIVITAT7/connection.py
+++ ACTIVITAT7/connection.py
@@ -1,13 +1,12 @@
 import psycopg2
 
 conn = psycopg2.connect(
-
         database="postgres",
         user='rdalmau',
         password='rdc123',
         host='localhost',
         port='5432' 
-     ) 
+) 
 
 
 connection = conn.cursor() 
