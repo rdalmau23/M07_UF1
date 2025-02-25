211a1d1a676f3b28c7f2fa255c32a00f3ec93202
Rafel Dalmau <145371879+rdalmau23@users.noreply.github.com>
2025-02-25 13:10:52 +0100

    Update read.py

--- ACTIVITAT9/read.py
+++ ACTIVITAT9/read.py
@@ -4,12 +4,8 @@ def read_users():
 
     conn = conn.connect_db
     cursor = conn.cursor()
-
     sql_read_users = "SELECT * FROM users"
-
     cursor.execute(sql_read_users)
-
     users = cursor.fetchall()
-
 
     return users
