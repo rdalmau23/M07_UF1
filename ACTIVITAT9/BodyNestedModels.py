0e95f00c2ee623e5c800b3c041efcd592ed3c28f
Rafel Dalmau <145371879+rdalmau23@users.noreply.github.com>
2025-02-25 13:08:13 +0100

    Update BodyNestedModels.py

--- ACTIVITAT9/BodyNestedModels.py
+++ ACTIVITAT9/BodyNestedModels.py
@@ -1,21 +1,17 @@
 from typing import Annotated
-
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
+    description: str | None = Field(default=None, title="The description of the item", max_length=300)
     name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
+    price: float = Field(gt=0, description="The price must be greater than zero")
 
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
-    results = {"item_id": item_id, "item": item}
-    return results
+    return {"item_id": item_id, "item": item}
