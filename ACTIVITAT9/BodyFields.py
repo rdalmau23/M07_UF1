4f7f92d86a8642e95ce413dae8382c42af7065f8
Rafel Dalmau <145371879+rdalmau23@users.noreply.github.com>
2025-02-25 13:08:02 +0100

    Update BodyFields.py

--- ACTIVITAT9/BodyFields.py
+++ ACTIVITAT9/BodyFields.py
@@ -1,20 +1,17 @@
-from typing import Annotated
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
+from typing import Annotated
 
 app = FastAPI()
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
     price: float = Field(gt=0, description="The price must be greater than zero")
+    description: str | None = Field(default=None, max_length=300, title="The description of the item")
     tax: float | None = None
 
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
-    results = {"item_id": item_id, "item": item}
-    return results
+    return {"item_id": item_id, "item": item}
