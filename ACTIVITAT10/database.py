08889492861722b09ee1f53daebe66cebeb18fb0
Rafel Dalmau <145371879+rdalmau23@users.noreply.github.com>
2025-02-25 13:16:54 +0100

    Update database.py

--- ACTIVITAT10/database.py
+++ ACTIVITAT10/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./penjat.db"  # Usa SQLite como base de datos
+DATABASE_URL = "sqlite:///./penjat.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
