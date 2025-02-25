4f599264bbedd3d9a9574855786d7ba0dfb20f45
Rafel Dalmau <145371879+rdalmau23@users.noreply.github.com>
2025-02-25 13:09:03 +0100

    Update BodyNestedModels.py

--- ACTIVITAT9/BodyNestedModels.py
+++ ACTIVITAT9/BodyNestedModels.py
@@ -6,10 +6,10 @@ app = FastAPI()
 
 
 class Item(BaseModel):
-    description: str | None = Field(default=None, title="The description of the item", max_length=300)
+    description: str | None = Field(default=None, title="Descripció del item", max_length=300)
     name: str
     tax: float | None = None
-    price: float = Field(gt=0, description="The price must be greater than zero")
+    price: float = Field(gt=0, description="EL preu ha de ser més de 0")
 
 
 @app.put("/items/{item_id}")
